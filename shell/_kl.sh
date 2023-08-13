d3cfe7c612c747c9f25958d7f335f41e9578b343
Asif <asif.log@gmail.com>
2023-08-13 08:20:41 +0500

    g.push

--- shell/_kl.sh
+++ shell/_kl.sh
@@ -2,11 +2,63 @@
 action=$1
 if [[ -z "$action" || "$action" == "--help" ]]; then
   echo "  ====== Key listener ======"
+  echo "                      build, build-release, update-dep, test, doc, format, publish-pkg"
+  echo "  r.  (run)           main, keys"
   echo "  g.                  commit, status, push"
-
-
 else
  case $action in
+
+  "r.main")
+        source cd-kl.sh
+        cargo run --bin main
+    ;;
+
+  "r.keys")
+        source cd-kl.sh
+        cargo run --bin keys
+    ;;
+
+  "build")
+        source cd-kl.sh
+        cargo build
+    ;;
+
+  "build-release")
+        source cd-kl.sh
+        cargo build --release
+    ;;
+
+  "update-dep")
+    source cd-kl.sh
+    cargo update
+  ;;
+
+  "test")
+    source cd-kl.sh
+    cargo test
+  ;;
+
+  "check")
+    source cd-kl.sh
+    cargo check
+  ;;
+
+  "doc")
+    source cd-kl.sh
+    cargo doc --open
+  ;;
+
+  "format")
+    source cd-kl.sh
+    cargo fmt
+  ;;
+
+
+  "public-pkg")
+    source cd-kl.sh
+    cargo publish
+  ;;
+
   "g.commit")
         source cd-kl.sh
         git add .
