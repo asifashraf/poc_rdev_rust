839a2dd633dd890011c7c19cfbb273834e0ea810
Asif <asif.log@gmail.com>
2023-08-14 12:20:55 +0500

    g.push

--- shell/_prr.sh
+++ shell/_prr.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 action=$1
 if [[ -z "$action" || "$action" == "--help" ]]; then
-  echo "  ====== poc rdev rust ======"
-  echo "  g.                  commit, status, push"
+  echo "====== poc rdev rust ======"
+  echo "[g.] GIT       "
+  echo "commit, status, push"
+  echo "[pta.] POC Tauri APP /poc/create-tauri-app-command"
+  echo "dev"
+  echo "==============================================================="
 else
  case $action in
 
@@ -11,18 +15,28 @@ else
         git add .
         git commit -m "$1"
     ;;
+
     "g.push")
         source cd-prr.sh
         git add .
         git commit -m "$1"
         git push
     ;;
+
   "g.status")
         source cd-prr.sh
         git status
     ;;
 
-  
+
+## cd poc/create-tauri-app-command
+    "pta.dev")
+        source cd-prr.sh
+        cd poc/create-tauri-app-command
+        yarn tauri dev
+    ;;
+
+
   *)
     # Handle unknown or missing arguments
     echo "Invalid or missing argument. Please specify a valid action or use --help for usage information."
